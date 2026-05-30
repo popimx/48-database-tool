d53f79565c36f635a53cd269c08aa7b56753743b
りさ <skpnx58@au.com>
2026-05-30 14:05:20 +0900

    Update stageList.js

--- config/stageList.js
+++ config/stageList.js
@@ -1,46 +1,127 @@
 export const STAGE_LIST = {
   akb48: [
-    "AKB48「ここからだ」",
-    "AKB48「RESET」",
-    "AKB48「ただいま 恋愛中」",
-    "AKB48「手をつなぎながら」"
+    {
+      name: "AKB48「ここからだ」",
+      file: "akb48_kokokarada"
+    },
+    {
+      name: "AKB48「RESET」",
+      file: "akb48_reset_2025"
+    },
+    {
+      name: "AKB48「ただいま 恋愛中」",
+      file: "akb48_tadaimarenaichu_2022"
+    },
+    {
+      name: "AKB48「手をつなぎながら」",
+      file: "akb48_tewotsunaginagara_2026"
+    }
   ],
 
   ske48: [
-    "チームS「僕の太陽」",
-    "チームS「ずっと君を探している」",
-    "チームKⅡ「シアターの女神」",
-    "チームE「RESET」",
-    "SKE48「制服の芽」",
-    "SKE48「可能性こそが未来」"
+    {
+      name: "チームS「僕の太陽」",
+      file: "ske48_bokunotaiyo_2025"
+    },
+    {
+      name: "チームS「ずっと君を探している」",
+      file: "ske48_zuttokimiwosagashiteiru"
+    },
+    {
+      name: "チームKⅡ「シアターの女神」",
+      file: "ske48_theaternomegami_2025"
+    },
+    {
+      name: "チームE「RESET」",
+      file: "ske48_reset_2025"
+    },
+    {
+      name: "SKE48「制服の芽」",
+      file: "ske48_seifukunome_2024"
+    },
+    {
+      name: "SKE48「可能性こそが未来」",
+      file: "ske48_kanoseikosogamirai"
+    }
   ],
 
   nmb48: [
-    "チームN「N ship」",
-    "チームM「Mのサイン」",
-    "チームBⅡ「僕のアオハル」",
-    "NMB48「情熱！めっちゃ楽舞友」",
-    "NMB48「青春！恋のDestination」",
-    "NMB48「天使のユートピア」",
-    "NMB48「なんばらえてぃー」",
-    "NMB48「さらば純情」",
-    "NMB48「ここにだって天使はいる」",
-    "NMB48「無限大ノック」"
+    {
+      name: "チームN「N ship」",
+      file: "nmb48_nship"
+    },
+    {
+      name: "チームM「Mのサイン」",
+      file: "nmb48_mnosign"
+    },
+    {
+      name: "チームBⅡ「僕のアオハル」",
+      file: "nmb48_bokunoaoharu"
+    },
+    {
+      name: "NMB48「情熱！めっちゃ楽舞友」",
+      file: "nmb48_jonetsumeccaragbuyu"
+    },
+    {
+      name: "NMB48「青春！恋のDestination」",
+      file: "nmb48_seishunkoinodestination"
+    },
+    {
+      name: "NMB48「天使のユートピア」",
+      file: "nmb48_tenshinoutopia"
+    },
+    {
+      name: "NMB48「なんばらえてぃー」",
+      file: "nmb48_nambariety"
+    },
+    {
+      name: "NMB48「さらば純情」",
+      file: "nmb48_sarabajunjo"
+    },
+    {
+      name: "NMB48「ここにだって天使はいる」",
+      file: "nmb48_kokonidattetenshihairu_2026"
+    },
+    {
+      name: "NMB48「無限大ノック」",
+      file: "nmb48_mugendaiknock"
+    }
   ],
 
   hkt48: [
-    "チームH「目撃者」",
-    "チームKⅣ「ここにだって天使はいる」",
-    "ひまわり組「逆上がり」"
+    {
+      name: "チームH「目撃者」",
+      file: "hkt48_mokugekisha"
+    },
+    {
+      name: "チームKⅣ「ここにだって天使はいる」",
+      file: "hkt48_kokonidattetenshihairu"
+    },
+    {
+      name: "ひまわり組「逆上がり」",
+      file: "hkt48_sakaagari"
+    }
   ],
 
   ngt48: [
-    "NGT48「おもいでいっぱい」",
-    "NGT48「シアターの女神」",
-    "NGT48「パジャマドライブ」"
+    {
+      name: "NGT48「おもいでいっぱい」",
+      file: "ngt48_omoideippai"
+    },
+    {
+      name: "NGT48「シアターの女神」",
+      file: "ngt48_theaternomegami"
+    },
+    {
+      name: "NGT48「パジャマドライブ」",
+      file: "ngt48_pajamadrive_2024"
+    }
   ],
 
   stu48: [
-    "STU48「アイドルの夜明け」"
+    {
+      name: "STU48「アイドルの夜明け」",
+      file: "stu48_idolnoyoake"
+    }
   ]
 };
